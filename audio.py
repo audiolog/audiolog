49ec5ad50156fd373f99272196f9bc5433198728
rjn945 <rjn945@gmail.com>
2009-08-19 23:59:35 +0000

    Added copyright and GPL notice all code files. Created roadmap.

--- audio.py
+++ audio.py
@@ -1,5 +1,21 @@
 # -*- coding: utf-8 -*-
 
+#  Azul Music Organizer
+#  Copyright © 2009  Matt Hubert <matt@cfxnetworks.com> and Robert Nagle <rjn945@gmail.com>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """Wrapper around the functionality provided by ReleaseManager.
 
 This file instantiates a ReleaseManager object then invokes its mainloop. If a
