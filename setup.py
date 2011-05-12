225d3ec9882b5b0efecf8a6c035653be92e727fb
Rob Nagle <rjn945@gmail.com>
2011-05-12 03:39:02 -0700

    Updated dependency checking in setup.py

--- setup.py
+++ setup.py
@@ -23,96 +23,111 @@
 import os
 import subprocess
 
-# First, try all of the dependency commands.
-
-dependencies = {"extract":  ["tar", "unzip", "bunzip2", "gunzip", "unrar", "unace"],
-                "decode":   ["oggenc", "oggdec", "mpc123", "mac", "lame"]}
-
-error = False
-fatal = False
-
-for category in dependencies:
-    for dependency in dependencies[category]:
-        try:
-            subprocess.Popen([dependency], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-        except OSError:
-            print "You do not have %s installed. You will not be able to %s some types of music." % (dependency, category)
-            print "Try going to http://www.google.com/search?q=%s to install what you need." % dependency
-            if dependency == "mac":
-                print "Because mac can be very difficult to find, we have provided the source under the 'libs/mac-3.99' directory."
-            error = True
-            print
-
-try:
-    import PyQt4
-except:
-    print "You do not have PyQt installed. You will not be able to run Audiolog."
-    print "Download it from:"
-    print "http://www.riverbankcomputing.co.uk/software/pyqt/intro"
-    print
-    error = True
-    fatal = True
-
-try:
-    import mutagen
-except:
-    print "You do not have Mutagen installed. You will not be able to tag your audio files."
-    print "Download it from:"
-    print "http://code.google.com/p/mutagen/"
-    print
-    error = True
-    fatal = True
-
-try:
-    import musicbrainz2
-except:
-    print "You do not have MusicBrainz2 installed. You will not be able to gather metainformation from the Internet."
-    print "Install it by running `python setup.py install` in the 'libs/python-musicbrainz2' folder included with Audiolog."
-    print "Alternatively, you can check it out from MusicBrainz's Subversion repository:"
-    print "svn checkout http://svn.musicbrainz.org/python-musicbrainz2/trunk python-musicbrainz2"
-    print "DO NOT download the python-musicbrainz2 0.6.0 library from their website or install it using your package manager."
-    print "0.6.0 is too out of date and will not work with Audiolog."
-    print
-    error = True
-    fatal = True
-
-try:
-    if musicbrainz2 and musicbrainz2.__version__ < '0.7.0':
-        print "Your copy of MusicBrainz2 is too old and will not work with Audiolog."
-        print "Please install the copy provided with Audiolog in the 'libs/python-musicbrainz2' folder and run `python setup.py install`."
+def checkDependencies():
+    """Determine whether essential and non-essential dependencies are present."""
+    
+    error = False
+    fatal = False
+    
+    # First, try all of the dependency commands.
+    cmds = {"extract": ["tar", "unzip", "bunzip2", "gunzip", "unrar", "unace"],
+            "decode":  ["oggenc", "oggdec", "mpc123", "mac", "lame", "wvunpack"]}
+    
+    for category in cmds:
+        for cmd in cmds[category]:
+            try:
+                subprocess.Popen([cmd], 
+                                 stdout = subprocess.PIPE, 
+                                 stderr = subprocess.PIPE)
+            except OSError:
+                print "You do not have %s installed." % cmd
+                print "You will not be able to %s some types of music." % category
+                #if cmd == "mac":
+                #    print "Because mac can be very difficult to find, we have",
+                #    print "provided the source in the 'libs/mac-3.99' directory."
+                #else:
+                print 'Search your package manager or Google for',
+                print '"%s" to install what you need.' % cmd
+                error = True
+                print
+    
+    try:
+        import PyQt4
+    except:
+        print "You do not have PyQt installed."
+        print "Without it, you cannot run Audiolog with a graphical interface."
+        print "Run Audiolog from the command line with the --no-gui option"
+        print "or download PyQt from:"
+        print "http://www.riverbankcomputing.co.uk/software/pyqt/intro"
+        print
+        error = True
+    
+    try:
+        import mutagen
+    except:
+        print "You do not have Mutagen installed."
+        print "Without it you cannot tag your audio files."
+        print "Download Mutagen from:"
+        print "http://code.google.com/p/mutagen/"
         print
         error = True
         fatal = True
-except NameError:
-    pass
-
-try:
-    # FIXME: Remove references to getPUID binary. Test whether pyofa works instead.
-    command = os.path.join(os.getcwd(), "getPUID")
-    p = subprocess.Popen([command, "tests/test.mp3"], stdout = subprocess.PIPE)
-    output = p.communicate()[0]  # Gets the output from the command
-    output = output.splitlines() # Turns it from a string to a tuple
     
-    if not output or (output[0] != "Success."):
-        print "getPUID does not work on your system, potentially because of an error message above."
-        print "You will not be able to use the MusicDNS service, and can disable \"Fetch PUIDs\" from Audiolog's configuration."
-        print "You will, however, still be able to sort most music."
+    try:
+        import musicbrainz2
+    except:
+        print "You do not have MusicBrainz2 installed."
+        print "You will not be able to gather metainformation from the Internet."
+        print "Search for and install the python-musicbrainz2 package in your",
+        print "package manager or download MusicBrainz2 from:"
+        print "http://musicbrainz.org/doc/PythonMusicBrainz2"
         print
         error = True
-except:
-    pass # This happened because we didn't have musicbrainz2, but that was already handled earlier.
-
-if not error:
-    print "Congratulations! Your system is ready to sort some music!"
-    print "You can run Audiolog by typing:"
-    print "`python audiolog.py`"
-    print "or clicking on \"audiolog.py\" in the src/ folder."
-    print "Happy sorting!"
-    print
-else:
-    print "Your system is missing some required libraries."
-    if fatal:
-        print "Unfortunately, Audiolog will not run correctly without these libraries, so please install them using the instructions above before running this program."
+        fatal = True
+    
+    try:
+        if musicbrainz2 and musicbrainz2.__version__ < '0.7.0':
+            print "Your copy of MusicBrainz2 is too old to work with Audiolog."
+            print "Update your python-musicbrainz2 package using your package",
+            print "manager or download the latest version from:"
+            print "http://musicbrainz.org/doc/PythonMusicBrainz2"
+            print
+            error = True
+            fatal = True
+    except NameError:
+        pass
+    
+    try:
+        import musicdns
+    except:
+        print "You do not have pyofa installed."
+        print "Without it, you cannot fingerprint audio files and search for"
+        print "matches in the MusicDNS database."
+        print "You can download pyofa from:"
+        print "http://furius.ca/pyofa/"
+        print
+        error = True
+        
+    # TODO: Possibly test that pyofa works.
+    
+    if not error:
+        print "Congratulations! Your system is ready to sort some music!"
+        print "You can run Audiolog by typing:"
+        print "`python audiolog.py`"
+        print "or clicking on \"audiolog.py\" in the src/ folder."
+        print "Happy sorting!"
+        print
     else:
-        print "Audiolog will still run, however depending on what you are missing, you may have substantially lower performance."
-        print "We highly recommend you install the missing libraries."
+        print "Your system is missing some of the libraries that Audiolog uses."
+        if fatal:
+            print "Unfortunately, Audiolog will not run without these libraries."
+            print "Please install them using the instructions above."
+        else:
+            print "Audiolog will still run but you will be not be able to",
+            print "perform all of the actions that Audiolog provides."
+            print "We recommend you install the missing libraries."
+    
+    return not fatal
+
+if __name__ == '__main__':
+    checkDependencies()
