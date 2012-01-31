e0a4c91c3d431822f75badc2cc1989810dac75dd
Rob Nagle <rjn945@gmail.com>
2012-01-30 23:37:07 -0800

    Move timeToStr to helpers; compare only certain fields; etc

--- test/test_metadata.py
+++ test/test_metadata.py
@@ -5,32 +5,7 @@ import subprocess
 import datetime
 from os.path import join
 
-from helpers import openAudioFile, toUnicode
-
-def timeToStr(t):
-    """Display time in format appropriate for its size."""
-    
-    def pluralize(unit, num):
-        return unit + "s" if num != 1 else unit
-    
-    days = t.days
-    hours, seconds = divmod(t.seconds, 60*60)
-    minutes, seconds = divmod(seconds, 60)
-    if days:
-        return "%d %s, %d %s" % (days, pluralize("day", days), 
-                                 hours, pluralize("hour", hours))
-    elif hours:
-        return "%d %s, %d %s" % (hours, pluralize("hour", hours),
-                                 minutes, pluralize("minute", minutes))
-    elif minutes:
-        return "%d %s, %d %s" % (minutes, pluralize("minute", minutes),
-                                 seconds, pluralize("second", seconds))
-    else:
-        seconds = seconds + t.microseconds/1000000.0
-        if seconds > 10:
-            return "%.1f seconds" % seconds
-        else:
-            return "%.2f seconds" % seconds
+from helpers import openAudioFile, toUnicode, timeToStr
 
 def log(msg):
     """Write msg to stdout and log file."""
@@ -47,6 +22,7 @@ def resultsToText(dirPath):
     filenames = ""
     metadata = ""
     indent = ""
+    fields = ["artist", "date", "album", "tracknumber", "tracktotal", "title"]
     for root, dirs, files in os.walk(dirPath):
         for dir in sorted(dirs):
             filenames += (indent + dir).ljust(50) + "\n"
@@ -54,8 +30,8 @@ def resultsToText(dirPath):
             filenames += (indent + file).ljust(50) + "\n"
             if os.path.splitext(file)[1].lower() in (".ogg", ".mp3"):
                 audioFile = openAudioFile(join(root, file))
-                md = "\n".join([field.ljust(16) + value[0].ljust(40) 
-                                for field, value in sorted(audioFile.items())])
+                md = "\n".join([field.ljust(16) + audioFile[field][0].ljust(40)
+                                for field in fields]
                 metadata += md + "\n\n"
         indent += "  "
     return toUnicode(filenames) + "\n" + toUnicode(metadata)
@@ -66,21 +42,21 @@ rejected = 0
 incorrect  = 0
 correct = 0
 
-samplesDirPath = join("..", "audiolog-samples")
+samplesDirPath = "samples"
 
 inputDirPath = join("testing", "input")
 outputDirPath = join("testing", "output")
 
 testStartTime = datetime.datetime.now()
 logFileName = testStartTime.strftime("%Y-%m-%d %H-%M") + ".txt"
-logFilePath = join(samplesDirPath, logFileName)
+logFilePath = join(samplesDirPath, "log", logFileName)
 logFile = open(logFilePath, "w")
 
 for sample in sorted(os.listdir(samplesDirPath)):
     # Make sure this is a numbered (e.g. "02") sample directory.
     if not sample.isdigit():
         continue
-    num = int(sample)
+    #num = int(sample)
     #if not num in (99,):
     #    continue
     attempts += 1
@@ -95,11 +71,15 @@ for sample in sorted(os.listdir(samplesDirPath)):
     sampleInputPath = join(samplesDirPath, sample, "input")
     sampleCorrectPath = join(samplesDirPath, sample, "correct")
     logPath = join(samplesDirPath, sample, "log")
+    outPath = join(logPath, "output")
+    errPath = join(logPath, "error")
+    metaPath = join(logPath, "meta")
     
     # Find the name of this sample and display it.
     genre, artist, year_album = [dirs[0] for root, dirs, files
-                                 in os.walk(sampleCorrectPath) if len(dirs) == 1]
-    name =  toUnicode("%s - %s" % (artist, year_album[7:]))
+                                 in os.walk(sampleCorrectPath) 
+                                 if len(dirs) == 1]
+    name = toUnicode("%s - %s" % (artist, year_album[7:]))
     if len(name) > 30:
         name = name[:27] + "..."
     log("\n%s %s " % (sample, name.ljust(30)))
@@ -111,17 +91,18 @@ for sample in sorted(os.listdir(samplesDirPath)):
     shutil.copytree(sourcePath, destPath)
     
     # Run Audiolog on this sample
-    out = open(join(logPath, "output"), "w+")
-    err = open(join(logPath, "error"), "w+")
-    cmd = ["audiolog", "--no-gui", "-s", outputDirPath, inputDirPath]
-    subprocess.Popen(cmd, stdout=out, stderr=err).wait()
+    cmd = ["python", "audiolog", "--no-gui", "-s", outputDirPath, inputDirPath]
+    with open(outPath, "w") as out, open(errPath, "w") as err:
+        subprocess.Popen(cmd, stdout=out, stderr=err).wait()
     
-    # Now let's get the error results (we'll use those) and close the files.
-    err.seek(0)
-    error = err.read()
-    out.close()
-    err.close()
+    with open(outPath, "r") as out, open(errPath, "r") as err:
+        output = out.read()
+        error = err.read()
     
+    metadata = [s for s in output.split("\n\n\n\n") if "Results:" in s]
+    with open(metaPath, "w") as meta:
+        meta.write("\n\n\n".join(metadata).strip().replace(" "*20, ""))
+
     # Check for whether the sample was sorted at all.
     if not os.listdir(outputDirPath):
         if "Traceback" in error:
